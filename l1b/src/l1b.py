6ec2f7f7094d6bc94e166c9e670aea78f78f4ad2
Noel Andolz Aguado <noel.andolz.aguado@gmail.com>
2026-09-17 17:23:16 +0200

    Session 1 finl commit

--- l1b/src/l1b.py
+++ l1b/src/l1b.py
@@ -63,6 +63,7 @@ class l1b(initL1b):
         :return: TOA in DN, equalized
         """
         #TODO
+        toa = (toa - eq_add) / eq_mult
         return toa
 
     def restoration(self,toa,gain):
@@ -73,6 +74,7 @@ class l1b(initL1b):
         :return: TOA in radiances [mW/sr/m2]
         """
         #TODO
+        toa = toa * gain
         self.logger.debug('Sanity check. TOA in radiances after gain application ' + str(toa[1,-1]) + ' [mW/m2/sr]')
 
         return toa
