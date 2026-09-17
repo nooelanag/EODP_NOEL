05860e14db3ff198ddb2771ac8d05b9acb7c2687
Noel Andolz Aguado <noel.andolz.aguado@gmail.com>
2026-09-17 20:28:21 +0200

    Second day last commit. spectralIntegration and rad2Irrad done.

--- ism/mainIsm.py
+++ ism/mainIsm.py
@@ -4,9 +4,11 @@
 from ism.src.ism import ism
 
 # Directory - this is the common directory for the execution of the E2E, all modules
-auxdir = r'C:\EODP\eodp_students\auxiliary'
-indir = r"C:\EODP\EODP_TER_2021_working\EODP-TS-ISM\input\gradient_alt100_act150" # small scene
-outdir = r"C:\EODP\EODP_TER_2021_working\EODP-TS-ISM\myoutput"
+auxdir = r'C:\\Users\\Noel\\Documents\\NOEL\\UNIVERSIDAD\\MASTER\\TELECO\\ACADEMICO\\26-27\\1er cuatri\\EODP\\EODP_NOEL\\auxiliary'
+indir = r"C:\\Users\\Noel\Documents\\NOEL\\UNIVERSIDAD\\MASTER\\TELECO\\ACADEMICO\\26-27\\1er cuatri\\EODP\\FILES\\EODP_TER_2021\\EODP_TER_2021\\EODP-TS-ISM\\input\\gradient_alt100_act150" # small scene
+outdir = r"C:\\Users\\Noel\\Documents\\NOEL\\UNIVERSIDAD\\MASTER\\TELECO\\ACADEMICO\\26-27\\1er cuatri\\EODP\\FILES\\EODP_TER_2021\\EODP_TER_2021\\EODP-TS-ISM\\output_noel"
+
+
 
 # Initialise the ISM
 myIsm = ism(auxdir, indir, outdir)
