022dce7b79fb1abe8046603e3efe09acdd4374ff
Noel Andolz Aguado <noel.andolz.aguado@gmail.com>
2026-09-17 17:47:33 +0200

    l1b_test created and not equlized folder for non_eq results creted

--- l1b/mainL1b.py
+++ l1b/mainL1b.py
@@ -4,9 +4,9 @@
 from l1b.src.l1b import l1b
 
 # Directory - this is the common directory for the execution of the E2E, all modules
-auxdir = r'C:\\Users\\Noel\\Documents\\NOEL\\UNIVERSIDAD\\MASTER\\TELECO + CIBER\\ACADEMICO\\26-27\\1er cuatri\\EODP\\EODP_NOEL\\auxiliary'
-indir = r"C:\\Users\\Noel\\Documents\\NOEL\\UNIVERSIDAD\\MASTER\\TELECO + CIBER\\ACADEMICO\\26-27\\1er cuatri\\EODP\\FILES\\EODP_TER_2021\\EODP_TER_2021\\EODP-TS-L1B\\input"
-outdir = r"C:\\Users\\Noel\\Documents\\NOEL\\UNIVERSIDAD\\MASTER\\TELECO + CIBER\\ACADEMICO\\26-27\\1er cuatri\\EODP\\FILES\\EODP_TER_2021\\EODP_TER_2021\\EODP-TS-L1B\\output_noel"
+auxdir = r'C:\\Users\\Noel\\Documents\\NOEL\\UNIVERSIDAD\\MASTER\\TELECO\\ACADEMICO\\26-27\\1er cuatri\\EODP\\EODP_NOEL\\auxiliary'
+indir = r"C:\\Users\\Noel\\Documents\\NOEL\\UNIVERSIDAD\\MASTER\\TELECO\\ACADEMICO\\26-27\\1er cuatri\\EODP\\FILES\\EODP_TER_2021\\EODP_TER_2021\\EODP-TS-L1B\\input"
+outdir = r"C:\\Users\\Noel\\Documents\\NOEL\\UNIVERSIDAD\\MASTER\\TELECO\\ACADEMICO\\26-27\\1er cuatri\\EODP\\FILES\\EODP_TER_2021\\EODP_TER_2021\\EODP-TS-L1B\\output_noel_no_equalized"
 
 # Initialise the ISM
 myL1b = l1b(auxdir, indir, outdir)
