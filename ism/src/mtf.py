23556b9582dac2bcf452eeb98eed6dedfb5dbe3e
Noel Andolz Aguado <noel.andolz.aguado@gmail.com>
2026-09-24 20:13:27 +0200

    End day 3

--- ism/src/mtf.py
+++ ism/src/mtf.py
@@ -70,7 +70,7 @@ class mtf:
 
         # Calculate the System MTF
         self.logger.debug("Calculation of the Sysmtem MTF by multiplying the different contributors")
-        Hsys = 1 # dummy
+        Hsys = Hdiff * Hdefoc * Hdet * Hsmear * Hmotion * Hwfe # dummy
 
         # Plot cuts ACT/ALT of the MTF
         self.plotMtf(Hdiff, Hdefoc, Hwfe, Hdet, Hsmear, Hmotion, Hsys, nlines, ncolumns, fnAct, fnAlt, directory, band)
